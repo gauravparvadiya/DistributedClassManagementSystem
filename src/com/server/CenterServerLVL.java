71eae224adca2efa802fb185e22d5c67c04cfbb3
Gaurav Parvadiya <gaurav.parvadiya@gmail.com>
2017-05-22 20:30:08 -0400

    Created packages and related java classes.

---
+++ src/com/server/CenterServerLVL.java
@@ -0,0 +1,10 @@
+package com.server;
+
+public class CenterServerLVL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/server/CenterServerMTL.java
@@ -0,0 +1,10 @@
+package com.server;
+
+public class CenterServerMTL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
