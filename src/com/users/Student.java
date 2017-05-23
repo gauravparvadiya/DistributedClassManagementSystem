531554acb2dfa64365df81c888cd55ec51ce5239
Gaurav Parvadiya <gaurav.parvadiya@gmail.com>
2017-05-22 21:09:04 -0400

    Student Class - Created Constructor, Getter and Setter methods Created Center Interface

--- src/com/users/Student.java
+++ src/com/users/Student.java
@@ -1,5 +1,54 @@
 package com.users;
 
+import java.util.ArrayList;
+
 public class Student {
 
+	private String fname;
+	private String lname;
+	private ArrayList<String> coursesRegistered;
+	private String status;
+	private String statusDueDate;
+	
+	public Student(String fname, String lname, ArrayList<String> coursesRegistered, String status,
+			String statusDueDate) {
+		//super();
+		this.fname = fname;
+		this.lname = lname;
+		this.coursesRegistered = coursesRegistered;
+		this.status = status;
+		this.statusDueDate = statusDueDate;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public ArrayList<String> getCoursesRegistered() {
+		return coursesRegistered;
+	}
+	public void setCoursesRegistered(ArrayList<String> coursesRegistered) {
+		this.coursesRegistered = coursesRegistered;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getStatusDueDate() {
+		return statusDueDate;
+	}
+	public void setStatusDueDate(String statusDueDate) {
+		this.statusDueDate = statusDueDate;
+	}
+	
+	
 }
