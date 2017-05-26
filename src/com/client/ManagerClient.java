8b5c1347b1f2da23449eef5367f0be552c869e28
Hirangi <Hirangi@lenovo>
2017-05-26 00:51:05 -0400

    Authenticate manager and connect to server

--- src/com/client/ManagerClient.java
+++ src/com/client/ManagerClient.java
@@ -41,25 +41,30 @@ public class ManagerClient {
 		ddo = new ArrayList<Manager>();
 		ddo.add(manager3);*/
 		File f=new File("res/manager.json");
-		System.out.println(f.canRead());
+		//System.out.println(f.canRead());
 		Reader reader =  new BufferedReader(new FileReader(f.getAbsolutePath())); //new FileReader("com/res/Manager.json");		
 		JsonParser parser = new JsonParser();
 		JsonArray array = parser.parse(reader).getAsJsonArray();
 
-		System.out.println(array.get(0));
-		System.out.println(array);
+		//System.out.println(array.get(0));
+		//System.out.println(array);
+		mtl=new ArrayList<Manager>();
+		managerHashMap = new HashMap<String, ArrayList<Manager>>();
 		
 		if (array != null) {
 			for (int i = 0; i < array.size(); i++) {
 				JsonObject object = (JsonObject) array.get(i);
-				System.out.println(object.get("managerID"));
-				
+				//System.out.println(object.get("managerID").getAsString());
 				Manager manager = new Manager(object.get("managerID").toString(),object.get("managerID").toString(),object.get("managerID").toString());
+				if(object.get("managerID").getAsString().substring(0, 3).equals("MTL")){
+					mtl.add(manager);
+				}
+				//System.out.println(mtl.size());
+				managerHashMap.put("MTL", mtl);
 			}
 		}
 		
-		managerHashMap = new HashMap<String, ArrayList<Manager>>();
-		managerHashMap.put("MTL", mtl);
+		//managerHashMap.put("MTL", mtl);
 		managerHashMap.put("LVL", lvl);
 		managerHashMap.put("DDO", ddo);
 		
@@ -71,10 +76,26 @@ public class ManagerClient {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the Manager ID : ");
 		String managerID=reader.readLine();
-		if(managerID.substring(0, 3).equals("MTL") && managerID.equals(managerClient.managerHashMap.get("MTL").get(0).getManagerID())){
-			Registry registry = LocateRegistry.getRegistry(2964);
-			Center stub=(Center)registry.lookup("MTLServer");
-		    stub.createSRecord("Hirangi", "Naik", "DSD", "ready", "4/5/17");
+		boolean result=false;
+		if(managerID.substring(0, 3).equals("MTL")){
+			for(int i=0;i<managerClient.managerHashMap.get("MTL").size();i++){
+				if(managerClient.managerHashMap.get("MTL").get(i).getManagerID().substring(1, 8).equals(managerID)){
+					result=true;
+				    break;
+				}
+				else{
+					result=false;
+				}
+					
+			}
+			if(result==true){
+				Registry registry = LocateRegistry.getRegistry(2964);
+				Center stub=(Center)registry.lookup("MTLServer");
+			    stub.createSRecord("Hirangi", "Naik", "DSD", "ready", "4/5/17");
+			}
+			else
+				System.out.println("Entered ID doesn't exists.");
+				System.exit(0);
 		}
 		else if(managerID.substring(0, 3).equals("LVL") && managerID.equals(managerClient.managerHashMap.get("LVL").get(0).getManagerID())){
 			Registry registry = LocateRegistry.getRegistry(1212);
@@ -88,7 +109,7 @@ public class ManagerClient {
 		}
 		else
 		{
-			System.out.println("Manager doesn't exists");
+			System.out.println("Manager ID should start with MTL/LVL/DDO");
 		}
 		
 	}
