2b588d0c15fd9a7cb0f24364405a217fa64309f6
Hirangi <Hirangi@lenovo>
2017-05-26 09:56:02 -0400

    Manager auth complete

--- src/com/client/ManagerClient.java
+++ src/com/client/ManagerClient.java
@@ -28,45 +28,37 @@ public class ManagerClient {
 	JSONParser parser;
 	
 	public ManagerClient() throws FileNotFoundException {
-		/*Manager manager1 = new Manager("MTL0001","Gaurav","Parvadiya");
-		Manager manager2 = new Manager("LVL0002","Hirangi","Naik");
-		Manager manager3 = new Manager("DDO0003","Jack","Reacher");*/
-		
-		/*mtl = new ArrayList<Manager>();
-		mtl.add(manager1);
-		
-		lvl = new ArrayList<Manager>();
-		lvl.add(manager2);
-		
-		ddo = new ArrayList<Manager>();
-		ddo.add(manager3);*/
+
 		File f=new File("res/manager.json");
-		//System.out.println(f.canRead());
 		Reader reader =  new BufferedReader(new FileReader(f.getAbsolutePath())); //new FileReader("com/res/Manager.json");		
 		JsonParser parser = new JsonParser();
 		JsonArray array = parser.parse(reader).getAsJsonArray();
-
-		//System.out.println(array.get(0));
-		//System.out.println(array);
+		
 		mtl=new ArrayList<Manager>();
+		lvl=new ArrayList<Manager>();
+		ddo=new ArrayList<Manager>();
+		
 		managerHashMap = new HashMap<String, ArrayList<Manager>>();
 		
 		if (array != null) {
 			for (int i = 0; i < array.size(); i++) {
 				JsonObject object = (JsonObject) array.get(i);
-				//System.out.println(object.get("managerID").getAsString());
 				Manager manager = new Manager(object.get("managerID").toString(),object.get("managerID").toString(),object.get("managerID").toString());
 				if(object.get("managerID").getAsString().substring(0, 3).equals("MTL")){
 					mtl.add(manager);
 				}
-				//System.out.println(mtl.size());
-				managerHashMap.put("MTL", mtl);
+				else if(object.get("managerID").getAsString().substring(0, 3).equals("LVL")){
+					lvl.add(manager);
+				}
+				else if(object.get("managerID").getAsString().substring(0, 3).equals("DDO")){
+					ddo.add(manager);
+				}
 			}
+			managerHashMap.put("MTL", mtl);
+			managerHashMap.put("LVL", lvl);
+			managerHashMap.put("DDO", ddo);
+
 		}
-		
-		//managerHashMap.put("MTL", mtl);
-		managerHashMap.put("LVL", lvl);
-		managerHashMap.put("DDO", ddo);
 		
 	}
 
@@ -97,15 +89,45 @@ public class ManagerClient {
 				System.out.println("Entered ID doesn't exists.");
 				System.exit(0);
 		}
-		else if(managerID.substring(0, 3).equals("LVL") && managerID.equals(managerClient.managerHashMap.get("LVL").get(0).getManagerID())){
-			Registry registry = LocateRegistry.getRegistry(1212);
-			Center stub=(Center)registry.lookup("LVLServer");
-		    stub.createSRecord("Hirangi", "Naik", "DSD", "ready", "4/5/17");
+		else if(managerID.substring(0, 3).equals("LVL")){
+			for(int i=0;i<managerClient.managerHashMap.get("LVL").size();i++){
+				if(managerClient.managerHashMap.get("LVL").get(i).getManagerID().substring(1, 8).equals(managerID)){
+					result=true;
+				    break;
+				}
+				else{
+					result=false;
+				}
+					
+			}
+			if(result==true){
+				Registry registry = LocateRegistry.getRegistry(1212);
+				Center stub=(Center)registry.lookup("LVLServer");
+			    stub.createSRecord("Hirangi", "Naik", "DSD", "ready", "4/5/17");
+			}
+			else
+				System.out.println("Entered ID doesn't exists.");
+				System.exit(0);
 		}
-		else if(managerID.substring(0, 3).equals("DDO") && managerID.equals(managerClient.managerHashMap.get("DDO").get(0).getManagerID())){
-			Registry registry = LocateRegistry.getRegistry(1111);
-			Center stub=(Center)registry.lookup("DDOServer");
-		    stub.createSRecord("Hirangi", "Naik", "DSD", "ready", "4/5/17");
+		else if(managerID.substring(0, 3).equals("DDO")){
+			for(int i=0;i<managerClient.managerHashMap.get("DDO").size();i++){
+				if(managerClient.managerHashMap.get("DDO").get(i).getManagerID().substring(1, 8).equals(managerID)){
+					result=true;
+				    break;
+				}
+				else{
+					result=false;
+				}
+					
+			}
+			if(result==true){
+				Registry registry = LocateRegistry.getRegistry(1111);
+				Center stub=(Center)registry.lookup("DDOServer");
+			    stub.createSRecord("Hirangi", "Naik", "DSD", "ready", "4/5/17");
+			}
+			else
+				System.out.println("Entered ID doesn't exists.");
+				System.exit(0);
 		}
 		else
 		{
